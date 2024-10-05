5d7ec398010875fde57eb75eae9537807dd35a38
varun-kumar06 <kumarmvarun39@gmail.com>
2024-10-05 15:55:33 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CarProject';
+  foods: Food[] = [
+    {
+      id: 1,
+      name: 'Dosa',
+      type: 'Veg',
+      price: 500,
+      imageUrl: 'https://images.pexels.com/photos/5560763/pexels-photo-5560763.jpeg?cs=srgb&dl=pexels-saveurssecretes-5560763.jpg&fm=jpg'
+    },
+    {
+      id: 2,
+      name: 'Ice-Briyani',
+      type: 'Veg',
+      price: 700,
+      imageUrl:'https://www.shutterstock.com/image-photo/boiled-rice-socked-overnight-water-260nw-2035163294.jpg'
+    },
+    {
+      id: 3,
+      name: 'Poori',
+      type: 'Mixed',
+      price: 600,
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMo1SjO6Pc4r--XqFJ8KzVPYnjInEeQhLhkA&s'
+    }
+  ];
+
+}
+
+export interface Food {
+  id: number;
+  name: string;
+  type: string;
+  price: number;
+  imageUrl: string;
 }
